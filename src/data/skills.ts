2b8aa786415e88def9d5689e50d783eb04d1e51f
William Moodhe <williammoodhe@gmail.com>
2024-08-19 02:38:12 -0400

    chore: Make skills load from an array

---
+++ src/data/skills.ts
@@ -0,0 +1,66 @@
+export const SKILLS = [
+	{
+		category: 'Languages',
+		skills: ['TypeScript', 'JavaScript', 'Python', 'C++', 'Lua', 'Java', 'PHP', 'C# and .NET'],
+	},
+	{
+		category: 'Back-end',
+		skills: ['Node', 'Express', 'Django', 'Laravel', 'NestJS'],
+	},
+	{
+		category: 'Front-end',
+		skills: ['React', 'Ember', 'HTML and CSS', 'NextJS', 'Preact', 'Vite', 'Storybook', 'Static-site generators'],
+	},
+	{
+		category: 'Web',
+		skills: ['Tailwind', 'Bootstrap', 'Material UI', 'Ant Design', 'Unstyled Components', 'Web Sockets', 'REST/CRUD', 'jquery', 'Webpack', 'babel'],
+	},
+	{
+		category: 'Databases',
+		skills: ['SQL', 'NoSQL', 'MySQL', 'PostgresDB', 'MongoDB', 'DocumentDB', 'DynamoDB', 'Redis'],
+	},
+	{
+		category: 'State management',
+		skills: ['Redux', 'Mobx', 'React Query', 'GraphQL', 'tRPC'],
+	},
+	{
+		category: 'Cloud',
+		skills: ['AWS (Amazon)', 'GCP (Google)', 'S3', 'Serverless', 'Lambda', 'EC2', 'ECS', 'SQS', 'IVS'],
+	},
+	{
+		category: 'Authentication',
+		skills: ['Auth0', 'Cognito'],
+	},
+	{
+		category: 'Testing',
+		skills: ['Jest', 'Cypress', 'React Testing Library'],
+	},
+	{
+		category: 'Source management',
+		skills: ['Git', 'SVN'],
+	},
+	{
+		category: 'Package management',
+		skills: ['npm', 'pnpm', 'yarn', 'pip', 'bun'],
+	},
+	{
+		category: 'Sandboxing',
+		skills: ['Docker', 'Kubernetes'],
+	},
+	{
+		category: 'Collaboration',
+		skills: ['Slack', 'Teams', 'Bitbucket', 'GitHub', 'Confluence'],
+	},
+	{
+		category: 'Creative',
+		skills: ['Photoshop'],
+	},
+	{
+		category: 'Game engines',
+		skills: ['Unreal', 'Unity', 'Source'],
+	},
+	{
+		category: 'Other',
+		skills: ['NGINX', 'CI/CD', 'Linux'],
+	},
+] as const
