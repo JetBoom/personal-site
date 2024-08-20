c9a71ff05e979eab22316d8535fe6792d2433c17
William Moodhe <williammoodhe@gmail.com>
2024-08-19 22:22:28 -0400

    refactor: Make experiences load from data

---
+++ src/data/experience.ts
@@ -0,0 +1,70 @@
+type ExperienceType = {
+    company: string
+    title: string
+    startDate: string
+    endDate: string
+	description: string
+    skills?: string[]
+	bullets?: string[]
+}
+
+export const EXPERIENCE: ExperienceType[] = [
+    {
+        company: 'SustainaBase',
+        title: 'Senior Software Engineer',
+        startDate: '05/2023',
+        endDate: '04/2024',
+		description: `Main engineer for both the React front-end and Node back-end.
+			Used modern TypeScript and cloud services (AWS).
+			We provided carbon tax accounting software for businesses.`,
+        skills: ['Redux', 'Tailwind', 'MySQL', 'MongoDB', 'Prisma', 'Auth0', 'Express', 'Serverless', 'S3'],
+    	bullets: [
+            'Automated customer data processing, reducing time needed from data engineers by 70%.',
+    		'Restructured legacy database, reducing page load times by 30%. Reduced storage cost by 50%.',
+        ],
+    },
+
+    {
+        company: 'Apricity Health',
+        title: 'Senior Software Engineer',
+        startDate: '12/2022',
+        endDate: '01/2023',
+        description: `Took ownership of the front-end and did back-end development for multiple React and Node apps.
+			Mentored and reviewed the code of entire team.
+			Resolved years of technical debt, eliminated security vulnerabilities, and refactored code to modern standards.`,
+		skills: ['TypeScript', 'Redux', 'GraphQL', 'Jest', 'DocumentDB', 'Serverless', 'S3', 'SQS', 'Docker', 'Material UI.'],
+    	bullets: [
+            'Created a data caching system, vastly reducing page load times by up to 90%.',
+    		'Resolved years of technical debt and refactored the code base size down by 30%, reducing required work from developers.',
+        ],
+    },
+
+	{
+        company: 'Mosaic Learning',
+        title: 'Software Engineer',
+        startDate: '01/2020',
+        endDate: '11/2022',
+        description: `Worked on many different projects that included Ember, React, Node, PHP, Java, or Laravel.
+			Chosen out of a company of roughly 15 engineers to spearhead the company migration from Ember, Java, and PHP to modern React and Node.
+			Took front-end ownership of a conferencing app which saw success during COVID and was featured in a post on the official AWS blog. Mentored other engineers, especially for front-end development.`,
+		skills: ['JavaScript', 'Express', 'Jest', 'Storybook', 'MongoDB', 'GraphQL', 'DynamoDB', 'S3', 'Lambda', 'Serverless', 'EC2', 'Docker', 'Cognito', 'IVS', 'MySQL', 'Java', 'Web Sockets', 'Material UI.'],
+    },
+
+    {
+		company: 'NoxFire Group',
+        title: 'Software Engineer',
+        startDate: '12/2018',
+        endDate: '12/2019',
+        description: `Worked on company and event websites. Conducted event broadcasts.`,
+		skills: ['JavaScript', 'Node', 'React', 'Express', 'UI/UX', 'Photoshop.'],
+    },
+
+	{
+        company: 'Altik',
+        title: 'Software Engineer',
+        startDate: '04/2017',
+        endDate: '12/2018',
+        description: `Worked on back-end and front-end of sportsbook software.`,
+		skills: ['JavaScript', 'Node', 'PHP', 'MySQL', 'HTML/CSS', 'jQuery'],
+    },
+] as const
