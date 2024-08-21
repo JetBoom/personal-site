c241081517e2517a4a646fda55f931e1789a95e9
William Moodhe <williammoodhe@gmail.com>
2024-08-20 21:59:38 -0400

    style: more readable font sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,15 +7,27 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				white: '#f8f9fa'
+				white: '#d8d1d0'
 			},
 			fontFamily: {
 				body: ['"Inter Tight Variable"', ...defaultTheme.fontFamily.sans]
 			},
 			gridTemplateColumns: {
 				list: 'repeat(auto-fill, minmax(400px, max-content))'
-			}
-		}
+			},
+			fontSize: {
+				xs: '14px',
+				sm: '16px',
+				base: '24px',
+				md: '24px',
+				lg: '32px',
+				xl: '48px',
+				'2xl': '60px',
+				'3xl': '100px',
+				'4xl': '150px',
+			},
+		},
+		
 	},
 	plugins: [require('@tailwindcss/typography')]
 }
