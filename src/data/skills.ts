e5c0bf2546d40f078d3d475719e545ad4be84224
William Moodhe <williammoodhe@gmail.com>
2024-08-26 08:58:58 -0400

    feat: add some skill icons to the top of page, make this pretty later

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,70 +1,92 @@
-export const SKILLS = [
+type SkillListType = {
+	category: string
+	skills: string[]
+}
+
+export const SKILLS: SkillListType[] = [
 	{
 		category: 'Languages',
-		skills: 'TypeScript, JavaScript, Python, C++, Lua, Java, PHP, C# and .NET',
+		skills: ['TypeScript', 'JavaScript', 'Python', 'C++', 'Lua', 'Java', 'PHP', 'C# and .NET'],
 	},
 	{
 		category: 'Back-end',
-		skills: 'Node, Express, Django, Laravel, NestJS',
+		skills: ['Node', 'Express', 'Django', 'Laravel', 'NestJS'],
 	},
 	{
 		category: 'Front-end',
-		skills: 'React, Ember, Vue, Svelte, Angular, HTML and CSS, ES6, NextJS, Preact, Vite, Storybook, Static-site generators',
+		skills: ['React', 'Ember', 'Vue', 'Svelte', 'Angular', 'HTML and CSS', 'ES6', 'NextJS', 'Preact', 'Vite', 'Storybook', 'Static-site generators'],
 	},
 	{
 		category: 'Web',
-		skills: 'Tailwind, Bootstrap, Material UI, Ant Design, Unstyled Components, Web Sockets, REST/CRUD, Accessibility, jquery, Webpack, babel',
+		skills: ['Tailwind', 'Bootstrap', 'Material UI', 'Ant Design', 'Unstyled Components', 'Web Sockets', 'REST/CRUD', 'Accessibility', 'jquery', 'Webpack', 'babel'],
 	},
 	{
 		category: 'Databases',
-		skills: 'SQL, NoSQL, MySQL, PostgresQL, MongoDB, DocumentDB, DynamoDB, Redis',
+		skills: ['SQL', 'NoSQL', 'MySQL', 'PostgresQL', 'MongoDB', 'DocumentDB', 'DynamoDB', 'Redis'],
 	},
 	{
 		category: 'State management',
-		skills: 'Redux, Mobx, React Query, GraphQL, tRPC',
+		skills: ['Redux', 'Mobx', 'React Query', 'GraphQL', 'tRPC'],
 	},
 	{
 		category: 'Cloud',
-		skills: 'AWS, GCP, Azure, S3, Serverless, Lambda, EC2, ECS, SQS, IVS, Route 53',
+		skills: ['AWS', 'GCP', 'Azure', 'S3', 'Serverless', 'Lambda', 'EC2', 'ECS', 'SQS', 'IVS', 'Route 53'],
 	},
 	{
 		category: 'Authentication',
-		skills: 'Auth0, Cognito',
+		skills: ['Auth0', 'Cognito'],
 	},
 	{
 		category: 'Testing',
-		skills: 'Jest, Cypress, React Testing Library',
+		skills: ['Jest', 'Cypress', 'React Testing Library'],
 	},
 	{
 		category: 'Source management',
-		skills: 'Git, SVN',
+		skills: ['Git', 'SVN'],
 	},
 	{
 		category: 'Package management',
-		skills: 'npm, pnpm, yarn, pip, bun',
+		skills: ['npm', 'pnpm', 'yarn', 'pip', 'bun'],
 	},
 	{
 		category: 'CI/CD',
-		skills: 'Docker, Kubernetes',
+		skills: ['Docker', 'Kubernetes'],
 	},
 	{
 		category: 'Collaboration',
-		skills: 'Slack, Teams, Bitbucket, GitHub, Confluence',
+		skills: ['Slack', 'Teams', 'Bitbucket', 'GitHub', 'Confluence'],
 	},
 	{
 		category: 'Creative',
-		skills: 'Photoshop',
+		skills: ['Photoshop'],
 	},
 	{
 		category: 'Game engines',
-		skills: 'Unreal, Unity, Source',
+		skills: ['Unreal', 'Unity', 'Source'],
 	},
 	{
 		category: 'Other',
-		skills: 'NGINX, CI/CD, Linux',
+		skills: ['NGINX', 'CI/CD', 'Linux'],
 	},
 	{
 		category: 'Industry-specific',
-		skills: 'HIPAA, SOC 2, FHIR',
+		skills: ['HIPAA', 'SOC 2', 'FHIR'],
 	},
+] as const
+
+export const MAIN_SKILLS: string[] = [
+	'TypeScript',
+	'JavaScript',
+	'Node.JS',
+	'React',
+	'AWS',
+	'Python',
+	'C++',
+	'Lua',
+	'C#',
+	'Java',
+	'Express',
+	'Ember',
+	'HTML',
+	'CSS',
 ] as const
