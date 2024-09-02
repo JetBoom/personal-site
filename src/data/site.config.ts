f56df8f0553463d1ae0659f08c25c268f8d4811c
William Moodhe <williammoodhe@gmail.com>
2024-09-02 15:15:55 -0400

    feat: add projects page which lists off github repos

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -1,6 +1,7 @@
 interface SiteConfig {
 	author: string
 	title: string
+	github: string
 	description: string
 	lang: string
 	ogLocale: string
@@ -9,6 +10,7 @@ interface SiteConfig {
 export const siteConfig: SiteConfig = {
 	author: 'William Moodhe',
 	title: 'William\'s Average Website',
+	github: 'jetboom',
 	description: 'My personal blog and portfolio.',
 	lang: 'en-US',
 	ogLocale: 'en_US',
