e9e776f8d1177d250774645902c020dec91b8a0b
William Moodhe <williammoodhe@gmail.com>
2024-09-07 17:05:02 -0400

    feat: convert education to Astro content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,9 @@
 import { PostsCollection } from '@/data/posts'
 import { ExperienceCollection } from '@/data/experience'
+import { EducationCollection } from '@/data/education'
 
 export const collections = {
 	posts: PostsCollection,
 	experience: ExperienceCollection,
+    education: EducationCollection,
 }
---
+++ src/data/education.ts
@@ -0,0 +1,21 @@
+import { defineCollection, getCollection, z } from 'astro:content'
+
+const schema = z.object({
+    degree: z.string(),
+    year: z.number(),
+    description: z.string().optional(),
+    url: z.string().optional(),
+    image: z.string().optional(),
+
+    hidden: z.boolean().optional(),
+})
+
+export const EducationCollection = defineCollection({ schema, type: 'data' })
+
+export type EducationType = z.infer<typeof schema>
+
+export async function getEducation() {
+	let items = await getCollection('education', item => !item.data.hidden) ?? []
+    items.sort((a, b) => b.data.year - a.data.year)
+	return items
+}
