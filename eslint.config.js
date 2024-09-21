a8e81b21cb7863cb881e23cfb52067ba15fda38f
William Moodhe <williammoodhe@gmail.com>
2024-09-21 06:38:26 -0400

    ci: add eslint-typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,34 @@
-import js from '@eslint/js'
-import eslintPluginAstro from 'eslint-plugin-astro'
+import eslintJS from '@eslint/js'
+import eslintTS from 'typescript-eslint'
+import eslintAstro from 'eslint-plugin-astro'
 
 export default [
-    //...js.configs.recommended,
-    ...eslintPluginAstro.configs.recommended,
+    eslintJS.configs.recommended,
+    ...eslintTS.configs.recommended,
+    ...eslintAstro.configs.recommended,
+
     {
         rules: {
-            semi: ['error', 'never'],
-            quotes: ['error', 'single'],
-            indent: ['error', 4],
+            'semi': [2, 'never'],
+            'indent': [2, 4],
+            'quotes': [2, 'single'],
+            'no-unused-vars': 1,
+            '@typescript-eslint/no-unused-vars': 0,
+            'prefer-const': 1,
+            '@typescript-eslint/ban-ts-comment': 0,
+            '@typescript-eslint/no-explicit-any': 1,
         },
+    },
+    {
         ignores: [
-            'dist',
-            'node_modules',
-            '.github',
-            '.changeset',
-            '.husky',
-            '.vscode',
+            'dist/*',
+            '.github/*',
+            '.changeset/*',
+            '.husky/*',
+            '.vscode/*',
+            '.astro/*',
+            'tina/*',
+            '*.cjs',
         ],
     },
 ]
